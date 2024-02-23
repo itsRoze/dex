67e3a66151fc4c1ac069faacbc126334de2f3167
itsRoze <elewis9989@gmail.com>
2024-02-23 17:03:15 -0500

    cleaner frontend

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { createResource, type Component } from "solid-js";
+import { IconSearch } from "./icons/search";
 
 const fetchData = async () => {
   console.log(import.meta.env.VITE_API_URL);
@@ -11,10 +12,18 @@ const fetchData = async () => {
 const App: Component = () => {
   const [data] = createResource(fetchData);
   return (
-    <>
-      <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-      <p class="text-center">{data()}</p>
-    </>
+    <main class="w-full h-full lg:max-w-4xl flex flex-col items-center">
+      <header class="flex gap-12 items-center justify-center w-full">
+        <img src="/logo.svg" class="w-32 h-32" />
+        <div class="flex items-center justify-stretch border-black border-2 px-2 w-1/2 rounded-md">
+          <IconSearch />
+          <input
+            type="text"
+            class="grow h-fit p-3 bg-transparent border-none focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+          />
+        </div>
+      </header>
+    </main>
   );
 };
 
