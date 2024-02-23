fe486499be1dfa188503288acb810fa9759cf84c
itsRoze <elewis9989@gmail.com>
2024-02-23 15:36:08 -0500

    add custom domain

--- stacks/web.ts
+++ stacks/web.ts
@@ -1,5 +1,6 @@
 import { StackContext, StaticSite, use } from "sst/constructs";
 import { API } from "./api";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 
 export function Web({ stack }: StackContext) {
   const api = use(API);
@@ -11,6 +12,20 @@ export function Web({ stack }: StackContext) {
     environment: {
       VITE_API_URL: api.url,
     },
+    customDomain:
+      stack.stage === "production"
+        ? {
+            domainName: "dex.roze.dev",
+            isExternalDomain: true,
+            cdk: {
+              certificate: Certificate.fromCertificateArn(
+                stack,
+                "DexRozeDev",
+                "arn:aws:acm:us-east-1:849356115576:certificate/f683b53d-ccb9-44a4-9b21-c1da66d9a441",
+              ),
+            },
+          }
+        : undefined,
   });
 
   stack.addOutputs({
